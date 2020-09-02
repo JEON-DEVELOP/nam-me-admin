66678bc6f81cdcd496dc853a921374a92e166655
JHB <jhyeonbin32@gmail.com>
2020-09-02 13:41:19 +0900

    postSql 연동 test

---
+++ src/main/java/com/example/nammeadmin/NamMeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.nammeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamMeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamMeAdminApplication.class, args);
+    }
+
+}
